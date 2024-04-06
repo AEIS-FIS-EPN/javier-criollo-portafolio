2ebe2c3ba0a8be18a9d4681dbe5b20b9c4750d86
Javier Criollo <javier.criollo@epn.edu.ec>
2024-04-05 23:45:34 -0500

    chore: Actualizar código JavaScript en App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <img src="https://via.placeholder.com/150" alt="Placeholder" className="card-image" />
+        <div className="card-content">
+          <h2>Nombre</h2>
+          <p>Javier Criollo</p>
+        </div>
+      </div>
+      <div className="card">
+        <img src="https://via.placeholder.com/150" alt="Placeholder" className="card-image" />
+        <div className="card-content">
+          <h2>Degree</h2>
+          <p>Computer Science</p>
+        </div>
+      </div>
+      
     </div>
   );
 }
